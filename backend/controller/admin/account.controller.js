7ec560a187228d58c71fa46e5deb83c1cb24fe42
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-14 10:27:38 +0700

    Hash mật khẩu

--- backend/controller/admin/account.controller.js
+++ backend/controller/admin/account.controller.js
@@ -1,22 +1,41 @@
+import bcrypt from "bcrypt";
 import AccountModel from "../../models/account.model.js";
 
-// [POST] /register
+// [POST] /auth/register
 export const register = async (req, res) => {
-  const isAccount = await AccountModel.findOne({ email: req.body.email });
+  try {
+    // Kiểm tra xem tài khoản đã tồn tại chưa
+    const isAccount = await AccountModel.findOne({ email: req.body.email });
 
-  if (isAccount) {
-    // Nếu tài khoản đã tồn tại
-    return res.status(400).json({
-      code: 400,
-      message: "Tài khoản đã tồn tại",
-    });
-  } else {
-    const account = new AccountModel(req.body);
-    account.save();
+    if (isAccount) {
+      // Nếu tài khoản đã tồn tại
+      return res.status(400).json({
+        code: 400,
+        message: "Tài khoản đã tồn tại",
+      });
+    } else {
+      // Mã hóa mật khẩu trước khi lưu
+      const saltRounds = 10; // Số rounds salt
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds); // Mã hóa mật khẩu
 
-    res.status(200).json({
-      code: 200,
-      message: "Đăng ký thành công",
+      // Tạo tài khoản mới với mật khẩu đã mã hóa
+      const account = new AccountModel({
+        ...req.body,
+        password: hashedPassword, // Thay thế mật khẩu gốc bằng mật khẩu đã mã hóa
+      });
+
+      await account.save();
+
+      res.status(200).json({
+        code: 200,
+        message: "Đăng ký thành công",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: 500,
+      message: "Đã có lỗi xảy ra trong quá trình đăng ký",
     });
   }
 };
