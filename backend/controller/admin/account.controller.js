fee83153b15cd1872b9d61454cc0af6daf009f83
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-14 10:30:01 +0700

    fix

--- backend/controller/admin/account.controller.js
+++ backend/controller/admin/account.controller.js
@@ -4,11 +4,9 @@ import AccountModel from "../../models/account.model.js";
 // [POST] /auth/register
 export const register = async (req, res) => {
   try {
-    // Kiểm tra xem tài khoản đã tồn tại chưa
     const isAccount = await AccountModel.findOne({ email: req.body.email });
 
     if (isAccount) {
-      // Nếu tài khoản đã tồn tại
       return res.status(400).json({
         code: 400,
         message: "Tài khoản đã tồn tại",
@@ -18,10 +16,9 @@ export const register = async (req, res) => {
       const saltRounds = 10; // Số rounds salt
       const hashedPassword = await bcrypt.hash(req.body.password, saltRounds); // Mã hóa mật khẩu
 
-      // Tạo tài khoản mới với mật khẩu đã mã hóa
       const account = new AccountModel({
         ...req.body,
-        password: hashedPassword, // Thay thế mật khẩu gốc bằng mật khẩu đã mã hóa
+        password: hashedPassword,
       });
 
       await account.save();
