c53964a6f307607b4a8c781155471fb7290b668f
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-16 08:20:22 +0700

    Tạo middlware để bảo mật cho các router private

--- backend/routes/admin/index.route.js
+++ backend/routes/admin/index.route.js
@@ -2,11 +2,12 @@ import productRoutes from "./product.route.js";
 import accountRoutes from "./account.route.js";
 import categoryRoutes from "./category.route.js";
 import tagRoutes from "./tag.route.js";
+import authenticateJWT from "../../middleware/authMiddleware.js";
 
 export default (app) => {
-  app.use("/product", productRoutes);
-  app.use("/account", accountRoutes);
+  app.use("/product", authenticateJWT, productRoutes);
+  app.use("/account", authenticateJWT, accountRoutes);
   app.use("/auth", accountRoutes);
-  app.use("/category", categoryRoutes);
-  app.use("/tag", tagRoutes);
+  app.use("/category", authenticateJWT, categoryRoutes);
+  app.use("/tag", authenticateJWT, tagRoutes);
 };
