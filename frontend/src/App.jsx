10e3ad3dc1767ccf63886d6a09d45190b5a27ea7
PhamTrung1204 <trungpham.31221024740@st.ueh.edu.vn>
2024-10-16 10:10:12 +0700

    Demo trang sản phẩm khi click vào ô danh mục sản phẩm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import TopAppBar from "./components/TopAppBar"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TopAppBar from "./components/TopAppBar"; // Thanh điều hướng
+import MenuProductPage from "./pages/MenuProductPage"; // Trang danh sách sản phẩm
 
 function App() {
   return (
-    <>
-      <TopAppBar/>
-    </>
-  )
+    <Router>
+      <TopAppBar />
+      <Routes>
+        <Route path="/" element={<MenuProductPage />} />
+        <Route path="/menu-products" element={<MenuProductPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
