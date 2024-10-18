d1758e3173907805a6a0580f574e97f9ac109feb
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-18 10:06:36 +0700

    Khi xóa address thì cũng xóa nó trong bảng Account

--- backend/controllers/client/address.controller.js
+++ backend/controllers/client/address.controller.js
@@ -1,3 +1,4 @@
+import AccountModel from "../../models/account.model.js";
 import Address from "../../models/adress.model.js";
 
 // [GET] /client/address
@@ -21,9 +22,24 @@ export const post = async (req, res) => {
   try {
     console.log(req.body);
 
-    const record = new Address(req.body);
+    // Lấy accountId từ request body (hoặc từ req.params nếu cần)
+    const { accountId, ...addressData } = req.body;
 
+    // Tạo bản ghi mới cho địa chỉ, bao gồm accountId
+    const record = new Address({ accountId, ...addressData });
+
+    // Lưu địa chỉ mới
     await record.save();
+
+    // Thêm ObjectId của địa chỉ vào mảng addresses trong tài khoản
+    await AccountModel.updateOne(
+      { _id: accountId },
+      {
+        $push: {
+          addresses: record._id, // Thêm ObjectId của địa chỉ vào mảng addresses
+        },
+      }
+    );
 
     res.status(200).json({
       code: 200,
@@ -61,17 +77,38 @@ export const edit = async (req, res) => {
 // [DELETE] /client/address/delete/:id
 export const deleteAddress = async (req, res) => {
   try {
+    // Tìm địa chỉ cần xóa
+    const address = await Address.findOne({ _id: req.params.id });
+
+    if (!address) {
+      return res.status(404).json({
+        code: 404,
+        message: "Địa chỉ không tồn tại",
+      });
+    }
+
+    // Xóa ObjectId của địa chỉ khỏi tài khoản
+    await AccountModel.updateOne(
+      { _id: address.accountId },
+      {
+        $pull: {
+          addresses: address._id, // Sử dụng _id của địa chỉ
+        },
+      }
+    );
+
+    // Xóa địa chỉ
     await Address.deleteOne({ _id: req.params.id });
 
     res.status(200).json({
       code: 200,
-      message: "Xóa địa chỉ mới thành công",
+      message: "Xóa địa chỉ thành công",
     });
   } catch (error) {
     res.status(400).json({
       code: 400,
       error: error.message,
-      message: "Xóa địa chỉ mới thất bại",
+      message: "Xóa địa chỉ thất bại",
     });
   }
 };
