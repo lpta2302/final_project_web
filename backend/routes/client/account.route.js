94d6543f583fffb20bfea4a2feb8e8a1a80c6a35
AriTannia <ngophucnguyen1976@gmail.com>
2024-10-18 09:39:19 +0700

    Tan finish Client -- Account, Tag, WishList, Product

---
+++ backend/routes/client/account.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import accountController from "../../controllers/client/account.controller.js";
+
+const accountRouter = Router();
+
+// Xem thông tin cá nhân
+accountRouter.get("/details/:id", accountController.showDetailsInfo);
+
+// Sửa thông tin cá nhân
+accountRouter.patch("/details/:id", accountController.updateDetailsInfo);
+
+export default accountRouter;
--- backend/routes/client/index.js
+++ backend/routes/client/index.js
@@ -1,8 +1,14 @@
 import voucherRoutes from "./voucher.route.js";
 import cartRoutes from "./cart.route.js";
+import productRoutes from "./product.route.js";
+import wishListRoutes from "./wishList.route.js";
+import accountRoutes from "./account.route.js";
 
 export default (app) => {
   app.use("/client/voucher", voucherRoutes);
   app.use("/client/cart", cartRoutes);
   app.use("/cart", cartRoutes);
+  app.use("/client/product", productRoutes);
+  app.use("/client/wishList", wishListRoutes);
+  app.use("/client/account", accountRoutes);
 };
