81c989c51ac1a2b68b6371e6cd6a6da68bcae874
PhamTrung1204 <trungpham.31221024740@st.ueh.edu.vn>
2024-10-19 11:26:17 +0700

    update

--- frontend/src/pages/HomePageClient.jsx
+++ frontend/src/pages/HomePageClient.jsx
@@ -10,13 +10,21 @@ import {
   Button,
   AppBar,
   Toolbar,
+  IconButton,
   Box,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { TopAppBar } from "../components";
 import { useNavigate } from "react-router-dom";
+import {
+  Category as CategoryIcon,
+  BrandingWatermark as BrandIcon,
+  KeyboardArrowDown,
+} from "@mui/icons-material";
 
 // Sample data for products
 const products = [
@@ -48,47 +56,20 @@ const products = [
       "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
     price: 1500,
   },
+  // Cập nhật id cho các sản phẩm còn lại
   {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
+    id: 5,
+    title: "Máy tính Jack 6 củ",
     image:
       "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
+    price: 1600,
   },
   {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
+    id: 6,
+    title: "Máy tính Jack 7 củ",
     image:
       "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
-  },
-  {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
-    image:
-      "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
-  },
-  {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
-    image:
-      "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
-  },
-  {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
-    image:
-      "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
-  },
-  {
-    id: 4,
-    title: "Máy tính Jack 5 củ",
-    image:
-      "https://phongvu.vn/cong-nghe/wp-content/uploads/sites/2/2020/09/Dell-XPS-15-1024x572.jpg",
-    price: 1500,
+    price: 1700,
   },
 ];
 
@@ -120,17 +101,17 @@ const banners = [
 ];
 
 const sliderSettings = {
-  dots: false, // No dots for a cleaner look
-  infinite: true, // Allows infinite scrolling
-  speed: 500, // Adjust speed of slide transition
-  slidesToShow: 4, // Number of visible items
-  slidesToScroll: 1, // Scroll 1 item at a time for smoother dragging
-  swipeToSlide: true, // Allow users to swipe and move between slides naturally
-  draggable: true, // Enable dragging on desktop
-  touchMove: true, // Allow touch move for mobile devices
-  cssEase: "cubic-bezier(0.25, 0.8, 0.25, 1)", // Smooth ease-in-out effect
-  adaptiveHeight: true, // Adjust height smoothly based on content
-  autoplay: false, // Option to autoplay slides
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  draggable: true,
+  touchMove: true,
+  cssEase: "cubic-bezier(0.25, 0.8, 0.25, 1)",
+  adaptiveHeight: true,
+  autoplay: false,
   responsive: [
     {
       breakpoint: 1024,
@@ -155,7 +136,7 @@ const sliderSettings = {
     },
   ],
 };
-// BannerSlider with Link to navigate
+
 const BannerSlider = () => {
   const settings = {
     dots: true,
@@ -176,7 +157,7 @@ const BannerSlider = () => {
           <Card>
             <CardMedia
               component="img"
-              height="500" // Tăng chiều cao banner
+              height="500"
               image={banner.image}
               alt={banner.title}
               sx={{ cursor: "pointer", marginTop: "10px" }}
@@ -193,7 +174,6 @@ const BannerSlider = () => {
   );
 };
 
-// ProductCard with Add to Cart functionality
 const ProductCard = ({ product, handleAddToCart }) => {
   return (
     <div style={{ padding: "0 10px", marginBottom: "10px" }}>
@@ -209,28 +189,26 @@ const ProductCard = ({ product, handleAddToCart }) => {
             alt={product.title}
           />
           <CardContent>
-            {/* Giới hạn chiều cao cho tiêu đề để tránh rớt chữ */}
             <Typography
               variant="h6"
               align="center"
               color="textPrimary"
               sx={{
-                height: "60px", // Giới hạn chiều cao tiêu đề
+                height: "60px",
                 overflow: "hidden",
                 textOverflow: "ellipsis",
                 display: "-webkit-box",
-                WebkitLineClamp: 2, // Giới hạn hiển thị 2 dòng
+                WebkitLineClamp: 2,
                 WebkitBoxOrient: "vertical",
               }}
             >
               {product.title}
             </Typography>
-
             <Typography
               variant="body2"
               color="text.secondary"
               align="center"
-              sx={{ height: "30px" }} // Đảm bảo chiều cao đồng nhất
+              sx={{ height: "30px" }}
             >
               Giá: ${product.price}
             </Typography>
@@ -251,172 +229,194 @@ const ProductCard = ({ product, handleAddToCart }) => {
   );
 };
 
-// SubNavbar component for categories and brands
 const SubNavbar = () => {
   const navigate = useNavigate();
 
-  // Handle category and brand click
+  const [categoryAnchorEl, setCategoryAnchorEl] = useState(null);
+  const [brandAnchorEl, setBrandAnchorEl] = useState(null);
+
+  const handleCategoryMouseEnter = (event) => {
+    setCategoryAnchorEl(event.currentTarget);
+  };
+
+  const handleBrandMouseEnter = (event) => {
+    setBrandAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setCategoryAnchorEl(null);
+    setBrandAnchorEl(null);
+  };
+
   const handleCategoryClick = (category) => {
     navigate(`/products?category=${category}`);
+    handleMenuClose();
   };
 
   const handleBrandClick = (brand) => {
     navigate(`/products?brand=${brand}`);
+    handleMenuClose();
   };
 
   return (
-    <Toolbar sx={{ backgroundColor: "#f4f4f4" }}>
-      <Box sx={{ flexGrow: 1 }}>
-        {/* NavItems by category */}
-        <Typography variant="h6" sx={{ display: "inline", marginRight: 2 }}>
-          Danh mục:
-        </Typography>
-        <Button onClick={() => handleCategoryClick("Laptop")} color="inherit">
-          Laptop
-        </Button>
-        <Button onClick={() => handleCategoryClick("PC")} color="inherit">
-          PC
-        </Button>
-        <Button onClick={() => handleCategoryClick("Màn hình")} color="inherit">
-          Màn hình
-        </Button>
-        <Button onClick={() => handleCategoryClick("Lâm")} color="inherit">
-          Lâm
-        </Button>
-        <Button onClick={() => handleCategoryClick("Ân")} color="inherit">
-          Ân
-        </Button>
-        <Button onClick={() => handleCategoryClick("Kiệt")} color="inherit">
-          Kiệt
-        </Button>
+    <Toolbar
+      sx={{
+        backgroundColor: "#fff",
+        borderBottom: "1px solid #ddd",
+        paddingY: "10px",
+        boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+      }}
+    >
+      <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+        {/* Danh mục */}
+        <IconButton
+          onMouseEnter={handleCategoryMouseEnter}
+          color="inherit"
+          sx={{
+            marginRight: 3,
+            "&:hover": {
+              color: "#ff4081",
+            },
+          }}
+        >
+          <CategoryIcon fontSize="medium" sx={{ marginRight: "5px" }} />
+          <Typography variant="h6" display="inline">
+            Danh mục
+          </Typography>
+          <KeyboardArrowDown />
+        </IconButton>
+        <Menu
+          anchorEl={categoryAnchorEl}
+          open={Boolean(categoryAnchorEl)}
+          onClose={handleMenuClose}
+          MenuListProps={{
+            onMouseLeave: handleMenuClose,
+          }}
+        >
+          <MenuItem onClick={() => handleCategoryClick("Laptop")}>
+            Laptop
+          </MenuItem>
+          <MenuItem onClick={() => handleCategoryClick("Chuột")}>
+            Chuột
+          </MenuItem>
+          <MenuItem onClick={() => handleCategoryClick("Tai nghe")}>
+            Tai nghe
+          </MenuItem>
+          <MenuItem onClick={() => handleCategoryClick("Loa")}>Loa</MenuItem>
+        </Menu>
 
-        {/* NavItems by brand */}
-        <Typography variant="h6" sx={{ display: "inline", marginLeft: 4 }}>
-          Thương hiệu:
-        </Typography>
-        <Button onClick={() => handleBrandClick("Dell")} color="inherit">
-          Dell
-        </Button>
-        <Button onClick={() => handleBrandClick("HP")} color="inherit">
-          HP
-        </Button>
-        <Button onClick={() => handleBrandClick("Apple")} color="inherit">
-          Apple
-        </Button>
-        <Button onClick={() => handleBrandClick("Acer")} color="inherit">
-          Acer
-        </Button>
-        <Button onClick={() => handleBrandClick("Asus")} color="inherit">
-          Asus
-        </Button>
-        <Button onClick={() => handleBrandClick("Lenovo")} color="inherit">
-          Lenovo
-        </Button>
+        {/* Thương hiệu */}
+        <IconButton
+          onMouseEnter={handleBrandMouseEnter}
+          color="inherit"
+          sx={{
+            "&:hover": {
+              color: "#ff4081",
+            },
+          }}
+        >
+          <BrandIcon fontSize="medium" sx={{ marginRight: "5px" }} />
+          <Typography variant="h6" display="inline">
+            Thương hiệu
+          </Typography>
+          <KeyboardArrowDown />
+        </IconButton>
+        <Menu
+          anchorEl={brandAnchorEl}
+          open={Boolean(brandAnchorEl)}
+          onClose={handleMenuClose}
+          MenuListProps={{
+            onMouseLeave: handleMenuClose,
+          }}
+        >
+          <MenuItem onClick={() => handleBrandClick("Dell")}>Dell</MenuItem>
+          <MenuItem onClick={() => handleBrandClick("Apple")}>Apple</MenuItem>
+          <MenuItem onClick={() => handleBrandClick("HP")}>HP</MenuItem>
+        </Menu>
       </Box>
     </Toolbar>
   );
 };
 
-// Home page component with cart management
-const HomePageClient = () => {
-  const [cart, setCart] = useState([]);
-
-  // Function to handle adding product to cart
-  const handleAddToCart = (product) => {
-    setCart([...cart, product]);
-    alert(`${product.title} đã được thêm vào giỏ hàng.`);
-  };
-
+const HomePage = ({ handleAddToCart }) => {
   return (
-    <>
-      {/* AppBar fixed with higher zIndex */}
-      <TopAppBar sx={{ zIndex: 1200 }} position="fixed" />
-
-      {/* Toolbar to create spacing equivalent to AppBar's height */}
-      <Toolbar />
-
-      {/* SubNavbar ngay dưới TopAppBar */}
+    <Container>
+      <TopAppBar />
       <SubNavbar />
+      <BannerSlider />
+      {/* Popular Products Section */}
+      <Typography
+        variant="h4"
+        align="left"
+        sx={{ margin: "30px 0", fontSize: "24px" }}
+      >
+        Sản phẩm thịnh hành
+      </Typography>
+      <Slider {...sliderSettings}>
+        {products.map((product) => (
+          <ProductCard
+            product={product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </Slider>
 
-      {/* Main content container */}
-      <Container>
-        {/* Banners Section */}
-        <BannerSlider />
+      {/* Promotion Products Section */}
+      <Typography
+        variant="h4"
+        align="left"
+        sx={{ margin: "30px 0", fontSize: "24px" }}
+      >
+        Sản phẩm khuyến mãi
+      </Typography>
+      <Slider {...sliderSettings}>
+        {products.map((product) => (
+          <ProductCard
+            product={product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </Slider>
 
-        {/* Popular Products Section */}
-        <Typography
-          variant="h4"
-          align="left"
-          sx={{ margin: "30px 0", fontSize: "24px" }}
-        >
-          Sản phẩm thịnh hành
-        </Typography>
-        <Slider {...sliderSettings}>
-          {products.map((product) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              handleAddToCart={handleAddToCart}
-            />
-          ))}
-        </Slider>
+      {/* Best-selling Products Section */}
+      <Typography
+        variant="h4"
+        align="left"
+        sx={{ margin: "30px 0", fontSize: "24px" }}
+      >
+        Laptop bán chạy
+      </Typography>
+      <Slider {...sliderSettings}>
+        {products.map((product) => (
+          <ProductCard
+            product={product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </Slider>
 
-        {/* Promotion Products Section */}
-        <Typography
-          variant="h4"
-          align="left"
-          sx={{ margin: "30px 0", fontSize: "24px" }}
-        >
-          Sản phẩm khuyến mãi
-        </Typography>
-        <Slider {...sliderSettings}>
-          {products.map((product) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              handleAddToCart={handleAddToCart}
-            />
-          ))}
-        </Slider>
-
-        {/* Best-selling Products Section */}
-        <Typography
-          variant="h4"
-          align="left"
-          sx={{ margin: "30px 0", fontSize: "24px" }}
-        >
-          Laptop bán chạy
-        </Typography>
-        <Slider {...sliderSettings}>
-          {products.map((product) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              handleAddToCart={handleAddToCart}
-            />
-          ))}
-        </Slider>
-
-        {/* Featured Products Section */}
-        <Typography
-          variant="h4"
-          align="left"
-          sx={{ margin: "30px 0", fontSize: "24px" }}
-        >
-          Sản phẩm nổi bật
-        </Typography>
-        <Slider {...sliderSettings}>
-          {products.map((product) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              handleAddToCart={handleAddToCart}
-            />
-          ))}
-        </Slider>
-      </Container>
-    </>
+      {/* Featured Products Section */}
+      <Typography
+        variant="h4"
+        align="left"
+        sx={{ margin: "30px 0", fontSize: "24px" }}
+      >
+        Sản phẩm nổi bật
+      </Typography>
+      <Slider {...sliderSettings}>
+        {products.map((product) => (
+          <ProductCard
+            product={product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </Slider>
+    </Container>
   );
 };
 
-export default HomePageClient;
+export default HomePage;
