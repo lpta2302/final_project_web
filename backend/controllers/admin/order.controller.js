f12453e06b22b781d1d9e517f0be51a7edfc5c24
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-21 08:57:46 +0700

    Get toàn bộ order

---
+++ backend/controllers/admin/order.controller.js
@@ -0,0 +1,109 @@
+import Order from "../../models/order.model.js";
+
+// [GET] /order
+export const index = async (req, res) => {
+  const order = await Order.find({});
+
+  res.json(order);
+};
+
+// [POST] /category/add
+export const detail = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const exitCategory = await Category.findOne({
+      categoryCode: req.body.categoryCode,
+    });
+    if (exitCategory) {
+      res.json({
+        code: 500,
+        message: "Mã category đã tồn tại!",
+      });
+      return;
+    }
+
+    const record = new Category(req.body);
+    await record.save();
+
+    res.json({
+      code: 200,
+      message: "Tạo category thành công",
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: "Thêm category thất bại",
+    });
+  }
+};
+
+// [PATCH] /category/edit
+export const edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Category.updateOne({ _id: id }, req.body);
+
+    res.json({
+      code: 200,
+      message: "Sửa category thành công",
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: "Sửa category thất bại",
+    });
+  }
+};
+
+// [DELETE] /category/delete/:id
+export const deleteOrder = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Category.deleteOne({ _id: id });
+
+    res.json({
+      code: 200,
+      message: "Xóa category thành công",
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: "Xóa category thất bại",
+    });
+  }
+};
+
+// [GET] /category/search
+export const search = async (req, res) => {
+  try {
+    // Khởi tạo filter là một đối tượng trống
+    let filter = {};
+
+    // Lấy giá trị từ query params
+    const { categoryCode, categoryName } = req.query;
+
+    // Kiểm tra nếu categoryCode có trong query
+    if (categoryCode) {
+      filter.categoryCode = categoryCode; // Tìm kiếm chính xác theo categoryCode
+    }
+
+    // Kiểm tra nếu categoryName có trong query
+    if (categoryName) {
+      filter.categoryName = { $regex: categoryName, $options: "i" }; // Tìm kiếm gần đúng theo categoryName, không phân biệt hoa thường
+    }
+
+    // Tìm kiếm với bộ lọc filter
+    const categories = await Category.find(filter);
+
+    // Trả về kết quả
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      message: "Tìm kiếm category thất bại",
+    });
+  }
+};
---
+++ backend/routes/admin/order.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as controller from "../../controllers/admin/order.controller.js";
+
+const router = Router();
+
+router.get("/", controller.index);
+// router.get("/user/:userId", controller.orderOfUser);
+router.post("/detail/:orderId", controller.detail);
+router.patch("/edit/:orderId", controller.edit);
+router.delete("/delete/:orderId", controller.deleteOrder);
+router.get("/search", controller.search);
+
+export default router;
