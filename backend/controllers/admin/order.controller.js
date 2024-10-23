04c8af043bdb3aa6f26d0fd656027997c3baa72c
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-23 17:26:07 +0700

    Thống kê order

--- backend/controllers/admin/order.controller.js
+++ backend/controllers/admin/order.controller.js
@@ -53,7 +53,7 @@ export const deleteOrder = async (req, res) => {
   try {
     const id = req.params.id;
 
-    const category = await Category.deleteOne({ _id: id });
+    await Order.deleteOne({ _id: id });
 
     res.status(200).json(true);
   } catch (error) {
@@ -84,7 +84,37 @@ export const search = async (req, res) => {
     const order = await Order.find(filter);
 
     // Trả về kết quả
-    res.status(200).json(categories);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json(false);
+  }
+};
+
+// [GET] /order/statistic
+export const statistic = async (req, res) => {
+  try {
+    let data = {};
+
+    // Sử dụng countDocuments để tối ưu truy vấn
+    const totalOrders = await Order.countDocuments({});
+    const orderComplete = await Order.countDocuments({
+      processStatus: "completed",
+    });
+    const orderPending = await Order.countDocuments({
+      processStatus: "pending",
+    });
+    const orderCancel = await Order.countDocuments({
+      processStatus: "cancelled",
+    });
+
+    // Gán dữ liệu vào đối tượng trả về
+    data.order = totalOrders;
+    data.orderComplete = orderComplete;
+    data.orderPending = orderPending;
+    data.orderCancel = orderCancel;
+
+    // Trả về kết quả
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json(false);
   }
--- backend/routes/admin/order.route.js
+++ backend/routes/admin/order.route.js
@@ -8,5 +8,6 @@ router.get("/user/:userId", controller.orderOfUser);
 router.get("/detail/:orderId", controller.detail);
 router.patch("/edit/:orderId", controller.edit);
 router.get("/search", controller.search);
+router.get("/statistic", controller.statistic);
 
 export default router;
