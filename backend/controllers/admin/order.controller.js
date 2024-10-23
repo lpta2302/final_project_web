c95008ee02a110b3d0265ccf5f20cf384c03aa4c
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-23 18:31:34 +0700

    Doanh thu theo ngày, tuần, tháng, năm

--- backend/controllers/admin/order.controller.js
+++ backend/controllers/admin/order.controller.js
@@ -119,3 +119,80 @@ export const statistic = async (req, res) => {
     res.status(500).json(false);
   }
 };
+
+// [GET] /order/statistic-revenue/:type
+export const statisticRevenue = async (req, res) => {
+  try {
+    const type = req.query.type;
+    let startDate, endDate;
+
+    // Lấy ngày hiện tại
+    const currentDate = new Date();
+
+    switch (type) {
+      case "day":
+        // Lấy thời gian bắt đầu và kết thúc của ngày
+        startDate = new Date(currentDate);
+        startDate.setHours(0, 0, 0, 0); // Bắt đầu từ 00:00:00 của hôm nay
+        endDate = new Date(currentDate);
+        endDate.setHours(23, 59, 59, 999); // Kết thúc 23:59:59 của hôm nay
+        break;
+
+      case "week":
+        // Lấy thời gian bắt đầu và kết thúc của tuần
+        startDate = new Date(currentDate);
+        startDate.setDate(currentDate.getDate() - currentDate.getDay()); // Chủ Nhật đầu tuần
+        startDate.setHours(0, 0, 0, 0); // Bắt đầu từ 00:00:00
+        endDate = new Date(startDate);
+        endDate.setDate(endDate.getDate() + 6); // Thứ Bảy cuối tuần
+        endDate.setHours(23, 59, 59, 999); // Kết thúc 23:59:59
+        break;
+
+      case "month":
+        // Lấy thời gian bắt đầu và kết thúc của tháng
+        startDate = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          1
+        ); // Ngày 1 của tháng
+        endDate = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth() + 1,
+          0
+        ); // Ngày cuối của tháng
+        endDate.setHours(23, 59, 59, 999); // Kết thúc 23:59:59
+        break;
+
+      case "year":
+        // Lấy thời gian bắt đầu và kết thúc của năm
+        startDate = new Date(currentDate.getFullYear(), 0, 1); // Ngày 1/1 của năm
+        endDate = new Date(currentDate.getFullYear(), 11, 31); // Ngày 31/12 của năm
+        endDate.setHours(23, 59, 59, 999); // Kết thúc 23:59:59
+        break;
+
+      default:
+        return res.status(400).json(false);
+    }
+
+    // Lấy ra các đơn hàng đã hoàn thành trong khoảng thời gian xác định dựa trên createdAt
+    const orders = await Order.find({
+      processStatus: "completed",
+      createdAt: { $gte: startDate, $lt: endDate },
+    });
+
+    // Kiểm tra xem có đơn hàng nào không
+    if (orders.length === 0) {
+      return res.status(200).json(0);
+    }
+
+    // Tính tổng doanh thu
+    const totalRevenue = orders.reduce(
+      (sum, order) => sum + order.totalAmount,
+      0
+    );
+
+    res.status(200).json({ totalRevenue });
+  } catch (error) {
+    res.status(500).json(false);
+  }
+};
