e4b9c8e2eea59c4aef545bb29105bb6b1a4d5ad5
lpta2302 <lpta2302@gmail.com>
2024-11-09 18:08:32 +0700

    remove env

--- frontend/src/api/myAxios.js
+++ frontend/src/api/myAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = (function () {
     let token = "";
-    axios.defaults.baseURL = 'http://localhost:4000';
+    axios.defaults.baseURL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
 
     const setBearerToken = (newToken) => {
         token = newToken;
