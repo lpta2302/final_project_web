fe997817d0e214816771533750b8c7c23d196740
lpta2302 <lpta2302@gmail.com>
2024-11-09 20:02:39 +0700

    deploying

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import routesAdmin from "./routes/admin/index.route.js";
-
+import expressSession from 'express-session'; 
 import routesClient from "./routes/client/index.js";
 
 import dotenv, { parse } from "dotenv";
@@ -17,6 +17,26 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan("common"));
+
+Object.defineProperty(expressSession.Cookie.prototype, 'sameSite', {
+  // sameSite cannot be set to `None` if cookie is not marked secure
+  get() {
+    return this._sameSite === 'none' && !this.secure ? 'lax' : this._sameSite;
+  },
+  set(value) {
+    this._sameSite = value;
+  }
+});
+
+app.use(expressSession({
+  secret: 'your_secret_key', // Set your secret key
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: 'auto', // Automatically set secure based on connection
+    sameSite: 'none' // Use None if secure; falls back to Lax if not secure
+  }
+}));
 
 app.use((req, res, next) => {
   const isHttps = req.protocol === "https"; // Check if the protocol is HTTPS
